643b5d04761f2ff7ac6cb1570ff57c4f0bda602e
Vuksan Simunović <vuk.simunovic@gmail.com>
2020-07-08 09:22:40 +0200

    Add deposit and withdrawal events.

--- Faucet.sol
+++ Faucet.sol
@@ -2,43 +2,44 @@
 pragma solidity ^0.4.22;
 
 contract owned {
-  address owner;
+    address owner;
+    
+    // Contract constructor: set owner
+    constructor() {
+        owner = msg.sender;
+    }
   
-  // Contract constructor: set owner
-  constructor() {
-    owner = msg.sender;
-  }
-  
-  // Access control modifier
-  modifier onlyOwner {
-    require(msg.sender == owner, "Only the contract owner can call this function");
-    _;
-  }
+    // Access control modifier
+    modifier onlyOwner {
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
+    }
 }
 
 contract mortal is owned {
-  // Contract destructor
-  function destroy() public onlyOwner {
-    selfdestruct(owner);
-  }
+    // Contract destructor
+    function destroy() public onlyOwner {
+        selfdestruct(owner);
+    }
 }
 
 // Our first contract is a faucet!
 contract Faucet is mortal {
+    event Withdrawal(address indexed to, uint amount);
+    event Deposit(address indexed from, uint amount);
 
-  // Give out ether to anyone who asks
-  function withdraw(uint withdraw_amount) public {
-  
-    // Limit withdrawal amount
-    require(withdraw_amount <= 0.1 ether);
-    
-    require(this.balance  >= withdraw_amount, "Insufficient balance in faucet for withdrawal request");
-    
-    // Send the amount to the address that requested it
-    msg.sender.transfer(withdraw_amount);
-  }
-  
-  // Accept any incoming payment
-  function () public payable {}
-  
+    // Give out ether to anyone who asks
+    function withdraw(uint withdraw_amount) public {
+        // Limit withdrawal amount
+        require(withdraw_amount <= 0.1 ether);
+        require(this.balance  >= withdraw_amount, "Insufficient balance in faucet for withdrawal request");
+        // Send the amount to the address that requested it
+        msg.sender.transfer(withdraw_amount);
+        emit Withdrawal(msg.sender, withdraw_amount);
+    }
+
+    // Accept any incoming payment
+    function () public payable {
+        emit Deposit(msg.sender, msg.value);
+    }
 }
