2cb5128be6f918a594378c3a326e6386f043af2f
Vuksan Simunović <vuk.simunovic@gmail.com>
2020-07-11 09:29:59 +0200

    Example of calling other contract

---
+++ Token.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.22;
+
+import "./Faucet.sol";
+
+contract Token is mortal {
+    Faucet _faucet;
+
+    constructor() {
+        _faucet = (new Faucet()).value(0.5 ether)();
+    }
+
+    function destroy() onlyOwner {
+        _faucet.destroy();
+    }
+}
